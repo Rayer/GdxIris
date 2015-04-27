464dfa0e5dad7df2bf6cdad315a020fc0d87f3f9
D.D <rayer.shih@gmail.com>
2015-04-28 01:18:42 +0800

    Added TracingBulletSpec

--- core/src/com/dr/iris/Iris.java
+++ core/src/com/dr/iris/Iris.java
@@ -13,8 +13,8 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.dr.iris.Objects.LinearBulletSpec;
 import com.dr.iris.Objects.ObjectManager;
+import com.dr.iris.Objects.TracingBulletSpec;
 import com.dr.iris.Render.IrisRenderer;
 import com.dr.iris.character.GameActor;
 import org.apache.logging.log4j.LogManager;
@@ -142,7 +142,8 @@ public class Iris extends ApplicationAdapter implements GestureDetector.GestureL
 
             if (actor.isHitDebugFrame(position.x, position.y)) {
                 Vector2 deltaPos = new Vector2(actor.getX() - mainActor.getX(), actor.getY() - mainActor.getY() + 5);
-                LinearBulletSpec spec = new LinearBulletSpec(new Vector2(mainActor.getX(), mainActor.getY()), deltaPos, 360.0f, 12.0f);
+                //LinearBulletSpec spec = new LinearBulletSpec(new Vector2(mainActor.getX(), mainActor.getY()), deltaPos, 360.0f, 12.0f);
+                TracingBulletSpec spec = new TracingBulletSpec(mainActor.getX(), mainActor.getY(), actor, 240.0f, 12.0f);
                 spec.setFrom(mainActor);
                 objectManager.createBulletObject(spec);
                 hit = true;
---
+++ core/src/com/dr/iris/Objects/TracingBulletSpec.java
@@ -0,0 +1,68 @@
+package com.dr.iris.Objects;
+
+import com.badlogic.gdx.math.Vector2;
+import com.dr.iris.character.GameActor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by Rayer on 4/28/15.
+ */
+public class TracingBulletSpec implements BulletSpec {
+
+    Logger logger = LogManager.getLogger(this.getClass());
+
+    GameActor target;
+    float ttl;
+    float speed;
+    boolean isAlive;
+    GameActor from;
+
+    Vector2 currentPos;
+
+    public TracingBulletSpec(float startX, float startY, GameActor targetActor, float speed, float ttl) {
+        currentPos = new Vector2(startX, startY);
+        target = targetActor;
+        this.speed = speed;
+        this.ttl = ttl;
+    }
+
+
+    @Override
+    public boolean isAlive() {
+        return true;
+    }
+
+    @Override
+    public boolean update(float delta) {
+
+        ttl -= delta;
+        if (ttl < 0) {
+            isAlive = false;
+            return false;
+        }
+
+        float deltaX = currentPos.x - target.getX();
+        float deltaY = currentPos.y - target.getY();
+
+        Vector2 velocity = new Vector2(deltaX, deltaY).setLength(speed * delta);
+        currentPos = currentPos.sub(velocity);
+        //logger.debug("Pos : " + currentPos);
+        return true;
+    }
+
+    @Override
+    public Vector2 getCurPos() {
+        return currentPos;
+    }
+
+    @Override
+    public GameActor getFrom() {
+        return from;
+    }
+
+    @Override
+    public void setFrom(GameActor object) {
+        from = object;
+    }
+}
